01b4f5dc7ab3aa1f6d252b0b5481adc2d2a4425e
Rebcca945 <106800841+Rebcca945@users.noreply.github.com>
2023-01-06 21:10:53 +0300

    Update 1-pack_web_static.py
    my sql

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
-
-from datetime import datetime
 from fabric.api import local
-from os.path import isdir
+from datetime import datetime
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """ Generate a tar archives """
+    date_recent = datetime.now().strftime("%Y%m%d%H%M%S")
+    path_ruth = "versions/web_static_{}.tgz".format(date_recent)
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(path_ruth))
+        return path_ruth
     except:
         return None
 Foot
